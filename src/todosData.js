e85d727f121d72b8f4e17342088643bdf2c07563
Tobias Hedin <tobias.hedin@gmail.com>
2019-05-09 13:45:27 +0200

    initial commit

---
+++ src/todosData.js
@@ -0,0 +1,45 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Give you up",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Let you down",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Run around",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Dessert you",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "Make you cry",
+        completed: false
+    },
+    {
+        id: 6,
+        text: "Say goodbye",
+        completed: false
+    },
+    {
+        id: 7,
+        text: "Tell a lie",
+        completed: false
+    },
+    {
+        id: 8,
+        text: "Hurt you",
+        completed: false
+    }
+]
+
+export default todosData;
+
