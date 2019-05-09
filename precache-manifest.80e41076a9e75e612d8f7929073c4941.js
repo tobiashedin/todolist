dfc1ebff5f8de2ada9aa7e37d37e49aa0a3915c8
Tobias Hedin <tobias.hedin@gmail.com>
2019-05-09 13:43:35 +0200

    Updates

---
+++ precache-manifest.80e41076a9e75e612d8f7929073c4941.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e64b6d2cde1e9db91d3d609142fed9",
+    "url": "/todolist-app/index.html"
+  },
+  {
+    "revision": "398118f1b9689ec7c445",
+    "url": "/todolist-app/static/css/main.8b91fd71.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/todolist-app/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "398118f1b9689ec7c445",
+    "url": "/todolist-app/static/js/main.df147221.chunk.js"
+  },
+  {
+    "revision": "4455dd72a5c5eafa8fb0",
+    "url": "/todolist-app/static/js/runtime~main.516f5cc3.js"
+  }
+]);
